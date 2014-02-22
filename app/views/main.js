9e05b9bb967b749d8787b3119a797b09e0c5e473
Gabriel Aponte <apontecolina@gmail.com>
2014-02-22 12:26:51 -0500

    New elements

--- app/views/main.js
+++ app/views/main.js
@@ -29,4 +29,10 @@ $(document).ready(function(){
           value: 37
         });
     });
+    
+    (function($) {
+        $(function() {
+            $("#accordion > div").accordion({ header: "h3", collapsible: true });
+        })
+    })(jQuery);
 });
