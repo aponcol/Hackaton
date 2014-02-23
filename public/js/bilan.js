59aa12b051b68602f85de4d4d28679dc7e65545d
Sonia Brami <7sonia@gmail.com>
2014-02-23 11:45:43 -0500

    d3

--- public/js/bilan.js
+++ public/js/bilan.js
@@ -19,7 +19,7 @@ function pieChart() {
                 radius = Math.min(width, height) / 2;
 
             var color = d3.scale.ordinal()
-                .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+                .range(["#E83F54", "#FFD41C", "#FCA11D", "#399E40"]);
 
             var arc = d3.svg.arc()
                 .outerRadius(radius - 10)
